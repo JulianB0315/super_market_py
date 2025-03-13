e73b0774f587f56ac3bd2cc1ee6966a15fa4a4ff
Julian Burga Bracamonte <julianburgabracamonte@gmail.com>
2025-03-13 13:21:39 -0500

    Update compras.py

--- chatbot-project-1/src/utils/compras.py
+++ chatbot-project-1/src/utils/compras.py
@@ -1,17 +1,15 @@
-from load_data import load_data
+from .load_data import load_data
 
 productos = load_data('chatbot-project-1/src/data/productos.csv')
 categorias = productos['categoria'].unique()
+
 def show_categories():
     print("Categorías disponibles:")
     for categoria in categorias:
         print(f"🔹 {categoria}")
+
 def products_by_categorie(categoria):
-    print(f"Productos en la categoría {categoria}:")
-    productos_categoria = productos[productos['categoria'] == categoria]
+    productos_categoria = productos[productos['categoria'].str.lower() == categoria.lower()]
     if productos_categoria.empty:
-        print("❌ No se encontraron productos en esta categoría")
-        return
-    for i, row in productos_categoria.iterrows():
-        print(f"🔸 {row['nombre']} | 💰 {row['precio']} | ⭐ {row['puntuacion']}/5")
-products_by_categorie('tec')
+        return None
+    return productos_categoria
