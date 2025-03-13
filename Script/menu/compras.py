527f3389b674d2f58c536d89de9006ee5d6358a2
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-13 12:19:49 -0500

    Enhance product recommendation display and add category exploration feature

--- Script/menu/compras.py
+++ Script/menu/compras.py
@@ -1,7 +1,5 @@
 import sys
 from pathlib import Path
-
-# Agregar el directorio Script al path
 script_dir = Path(__file__).parent.parent
 sys.path.append(str(script_dir))
 from utils.load_data import load_data
@@ -10,7 +8,21 @@ from models import recommendations
 def showProducts (id_user):
     productos = load_data('Script/data/productos.csv')
     recomendaciones = recommendations.modelo.recommend(id_user, 5)
-    print(recomendaciones)
+    print("🎯 Recomendaciones personalizadas para el usuario:")
+    print("🎯 Recomendaciones personalizadas para el usuario:")
+    for idx, producto in recomendaciones.iterrows():
+        print(f"Producto: {producto['nombre']}, Categoría: {producto['categoria']}, Precio: ${producto['precio']:.2f}")
 
+    print("\n📜 Categorías:")
+    for categoria in productos['categoria'].unique():
+        print(f"• {categoria}")
+    opcion = input("\n🔍 ¿Deseas ver más acerca de alguna categoría? (s/n): ")
+    if opcion == 's':
+        categoria = input("🔍 Ingresa la categoría que deseas ver: ")
+        ver_categoria(id_user,categoria)
+    else:
+        print("👋 ¡Hasta luego!")
+        menu(id_user)
+    
 if __name__ == "__main__":
     showProducts(1)
