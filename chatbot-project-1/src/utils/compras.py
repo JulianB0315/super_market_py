0a19fde46f69b71488df191e44bc5de502f95308
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-13 12:56:48 -0500

    Refactor showCategories function and add products_by_categorie to display products by category

--- chatbot-project-1/src/utils/compras.py
+++ chatbot-project-1/src/utils/compras.py
@@ -1,8 +1,17 @@
 from load_data import load_data
 
-def showCategories():
-    productos = load_data('chatbot-project-1/src/data/productos.csv')
-    categorias = productos['categoria'].unique()
+productos = load_data('chatbot-project-1/src/data/productos.csv')
+categorias = productos['categoria'].unique()
+def show_categories():
     print("Categorías disponibles:")
     for categoria in categorias:
         print(f"🔹 {categoria}")
+def products_by_categorie(categoria):
+    print(f"Productos en la categoría {categoria}:")
+    productos_categoria = productos[productos['categoria'] == categoria]
+    if productos_categoria.empty:
+        print("❌ No se encontraron productos en esta categoría")
+        return
+    for i, row in productos_categoria.iterrows():
+        print(f"🔸 {row['nombre']} | 💰 {row['precio']} | ⭐ {row['puntuacion']}/5")
+products_by_categorie('tec')
